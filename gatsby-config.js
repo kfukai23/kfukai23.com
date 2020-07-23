a16b2e86ede31971dbf63d51d785c18c9ff8021f
Kaito Fukai <k.fukai@everyleaf.com>
2020-07-23 20:18:43 +0900

    著者 bio を非表示にする
    本当はコメントアウトではなく削除してしまいたい
    が、そのうちまた表示したくなるかもしれない && シュッと追加できるほど React わからないので一旦残しておく

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `kfukai23.com`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `kfukai23`,
+      summary: `ソフトウェアエンジニアをしています`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    description: ``,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
     social: {
       twitter: `kylemathews`,
